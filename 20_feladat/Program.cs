0a93d1fea8e9d9f2b696e4dc438f23edd86a8351
KopaszB <145481444+KopaszB@users.noreply.github.com>
2025-01-31 11:57:09 +0100

    Két szám közül melyik a nagyobb

---
+++ 20_feladat/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_feladat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot: ");
+            int szam = int.Parse(Console.ReadLine());
+
+            if (szam%2==0)
+            {
+                Console.WriteLine($"A(z) {szam} páros.");
+            }
+            else
+            {
+                Console.WriteLine($"A(z) {szam} páratlan.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ melyikNagyobb/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace melyikNagyobb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Add meg az első számot: ");
+            int szam1 = int.Parse(Console.ReadLine());
+            Console.Write("Add meg a második számot: ");
+            int szam2 = int.Parse(Console.ReadLine());
+            if (szam1>szam2)
+            {
+                Console.WriteLine("Az első szám a nagyobb.");
+            }
+            else
+            {
+                Console.WriteLine("A második szám a nagyobb.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
